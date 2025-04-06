4a78e6e6dfd2a952692a664573f795f8a9ab1931
Egor-Ubuntu <227mr123@mail.ru>
2025-04-06 20:18:29 +0300

    changed 2x2-def page

---
+++ default-scripts.js
@@ -0,0 +1,17 @@
+// Динамические частицы
+function createParticles() {
+    const particlesContainer = document.querySelector('.particles');
+    for (let i = 0; i < 10; i++) {
+        const particle = document.createElement('div');
+        particle.className = 'particle';
+        particle.style.left = Math.random() * 100 + '%';
+        particle.style.top = Math.random() * 100 + '%';
+        particle.style.animationDuration = 15 + Math.random() * 15 + 's';
+        particle.style.animationDelay = Math.random() * 5 + 's';
+        particle.style.width = 2 + Math.random() * 3 + 'px';
+        particle.style.height = particle.style.width;
+        particlesContainer.appendChild(particle);
+    }
+}
+
+createParticles();
